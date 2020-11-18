c6a5b1a537283f701f97dc233b2b60213dca6c26
Bruno Thuma <40045005+BrunoThuma@users.noreply.github.com>
2020-11-18 15:50:01 -0300

    Incluindo Questão 10

---
+++ Questao 10 - Swap/README.md
@@ -0,0 +1,7 @@
+# Questão 10
+## Conteudo
+- Codigo fonte em C++ no arquivo questao10.cpp
+- Executavel questao10 compilado a partir do arquivo cpp (testado em linux shell com cygwin)
+## Forma de Usar
+- Será solicitado uma string para ser invertida
+- O programa imprimirá na tela a string após ser invertida
---
+++ Questao 10 - Swap/questao10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Usar como parametro o ponteiro da string passada
+// dessa forma podemos alterala sem usar segundo buffer
+void invert(string &ptrString){
+    for(int i = 0; i < (ptrString.size()/2); i++){
+        // Armazenar o char na ultima posicao em variavel temporaria
+        char temp = ptrString[ptrString.size()-(i + 1)];
+        // Substituir ultimo char pelo primeiro
+        ptrString[ptrString.size()-(i+1)] = ptrString[i];
+        // Substituir primeiro char pelo ultimo, armazenado na variavel temporaria
+        ptrString[i] = temp;
+    }
+}
+
+int main(){
+    // Curisosidade: "subi no onibus" ao contrario fica "subi no onibus"
+    string example = "SubiNoOnibus";
+    string usrInput;
+    cout << "Digite a string a ser invertida \n";
+    getline(cin, usrInput);
+    cout << "String original: " << usrInput << "\n";
+    invert(usrInput);
+    cout << "String invertida: " << usrInput << "\n";
+}
